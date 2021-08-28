98eb8a9865d242f4a241f651ea23b0c843640264
Anne-Marie Nault <anne.ma@gmail.com>
2021-08-28 11:40:21 -0400

    prepared app for heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Article.destroy_all
+
 10.times do
   Article.create!(
     title: Faker::Book.title,
-    content: Faker::Marketing.buzzwords
+    content: Faker::Lorem.paragraph(sentence_count: 10, supplemental: true, random_sentences_to_add: 4)
   )
 end
