faee7bf6b6e72f46bc9c2704508dd539ebcb0e9d
Anne-Marie Nault <anne.ma@gmail.com>
2021-08-28 11:55:11 -0400

    small fix to back links

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_05_11_183103) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
